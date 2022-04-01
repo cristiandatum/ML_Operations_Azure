440485ce85350d96eaf088e2281620d0e1e0f63c
cristiandatum <cristian.alberch@outlook.com>
2022-04-01 18:54:01 +0200

    revised endpoint using azure deploy

--- endpoint.py
+++ endpoint.py
@@ -1,43 +1,69 @@
-import requests
+import urllib.request
 import json
+import os
+import ssl
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+# Request data goes here
+data = {
+    "Inputs": {
+        "data":
+        [
+            {
+                "age": "0",
+                "job": "example_value",
+                "marital": "example_value",
+                "education": "example_value",
+                "default": "example_value",
+                "housing": "example_value",
+                "loan": "example_value",
+                "contact": "example_value",
+                "month": "example_value",
+                "day_of_week": "example_value",
+                "duration": "0",
+                "campaign": "0",
+                "pdays": "0",
+                "previous": "0",
+                "poutcome": "example_value",
+                "emp.var.rate": "0",
+                "cons.price.idx": "0",
+                "cons.conf.idx": "0",
+                "euribor3m": "0.0",
+                "nr.employed": "0"
+            },
+        ]
+    },
+    "GlobalParameters": {
+        "method": "predict"
+    }
+}
+
+body = str.encode(json.dumps(data))
 
 # URL for the web service
-
 scoring_uri = 'http://7386b71a-601c-48fb-b109-c47416fa192a.westeurope.azurecontainer.io/score'
 
 # If the service is authenticated, set the key or token
-key = "7BhaMreasi8hCGNhUHRqDccnZHHyRpl1"
+api_key = "7BhaMreasi8hCGNhUHRqDccnZHHyRpl1"
 
-# Two sets of data to score, so we get two results back
-data = {
-    "data": [
-        {
-            "instant": 1,
-            "date": "2013-01-01 00:00:00,000000",
-            "season": 1,
-            "yr": 0,
-            "mnth": 1,
-            "weekday": 6,
-            "weathersit": 2,
-            "temp": 0.344167,
-            "atemp": 0.363625,
-            "hum": 0.805833,
-            "windspeed": 0.160446,
-            "casual": 331,
-            "registered": 654,
-        },
-    ]
-}
-# Convert to JSON string
-input_data = json.dumps(data)
-with open("data.json", "w") as _f:
-    _f.write(input_data)
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-# Set the content type
-headers = {"Content-Type": "application/json"}
-# If authentication is enabled, set the authorization header
-headers["Authorization"] = f"Bearer {key}"
+req = urllib.request.Request(scoring_uri, body, headers)
 
-# Make the request and display the response
-resp = requests.post(scoring_uri, input_data, headers=headers)
-print(resp.json())
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(error.read().decode("utf8", 'ignore'))
